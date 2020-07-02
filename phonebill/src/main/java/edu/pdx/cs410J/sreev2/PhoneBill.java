f4b212b21faad9ee84f925b0d3e60e98f84ba635
Sree-Vandana <sreevandana.sv@gmail.com>
2020-07-01 22:27:15 -0700

    Modified Phonebill class and documented it.

--- phonebill/src/main/java/edu/pdx/cs410J/sreev2/PhoneBill.java
+++ phonebill/src/main/java/edu/pdx/cs410J/sreev2/PhoneBill.java
@@ -6,31 +6,63 @@ import edu.pdx.cs410J.AbstractPhoneCall;
 import java.util.ArrayList;
 import java.util.Collection;
 
-public class PhoneBill extends AbstractPhoneBill{
+public class PhoneBill extends AbstractPhoneBill<PhoneCall>{
 
+    /**
+     * customer Name of type <code>String</code>
+     * */
     private String customerName;
-    private ArrayList<PhoneCall> phoneCalls = new ArrayList<>();
 
-    public PhoneBill() {
+    /**
+     * <code>ArrayList</code> of type <code>PhoneCall</code>
+     * Holds all phone calls records.
+     * For project 1, it holds one <code>PhoneCall</code> record.
+     * */
+    final private ArrayList<PhoneCall> phoneCalls= new ArrayList<>();
+
+    /**
+     * Constructor for PhoneBill class, with no parameters.
+     * */
+    public PhoneBill(){
         super();
     }
 
-    public PhoneBill(String name, PhoneCall pcalls){
+    /**
+     * Constructor of PhoneBill class, with parameters
+     * @param name
+     *          Customer Name
+     * @param newPhoneCallRecord
+     *          This is Customers new/first phone call record.
+     *
+     * */
+    public PhoneBill(String name, PhoneCall newPhoneCallRecord){
         super();
         customerName = name;
-        phoneCalls.add(pcalls);
+        addPhoneCall(newPhoneCallRecord);
     }
 
+    /**
+     * <method>getCustomer</method> has no parameter
+     * @return customerName
+     * */
     @Override
     public String getCustomer() {
         return customerName;
     }
 
+    /***
+     * <method>addPhoneCall</method> adds a new record to the <type>ArrayList</type>
+     * @param phoneCall
+     *          phoneCall is of type <class>PhoneCall</class>
+     */
     @Override
-    public void addPhoneCall(AbstractPhoneCall abstractPhoneCall) {
-
+    public void addPhoneCall(PhoneCall phoneCall) {
+        phoneCalls.add(phoneCall);
     }
 
+    /**
+     * <method>getPhoneCalls</method> returns <class>Collection</class> of phoneCall records.
+     * */
     @Override
     public Collection<PhoneCall> getPhoneCalls() {
         return phoneCalls;
