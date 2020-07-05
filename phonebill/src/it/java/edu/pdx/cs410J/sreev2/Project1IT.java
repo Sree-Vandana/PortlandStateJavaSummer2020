d5e44e3ae4dacb46623f2f3d40600f3657b88118
Sree-Vandana <sreevandana.sv@gmail.com>
2020-07-04 17:13:50 -0700

    Project1 code is written and regex for validating cmd line arguments.

--- phonebill/src/it/java/edu/pdx/cs410J/sreev2/Project1IT.java
+++ phonebill/src/it/java/edu/pdx/cs410J/sreev2/Project1IT.java
@@ -4,9 +4,13 @@ import edu.pdx.cs410J.InvokeMainTestCase;
 import org.junit.Ignore;
 import org.junit.Test;
 
+import java.util.IllegalFormatException;
+import java.util.regex.Pattern;
+
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Tests the functionality in the {@link Project1} main class.
@@ -46,43 +50,36 @@ public class Project1IT extends InvokeMainTestCase {
       assertThat(result.getTextWrittenToStandardOut(), containsString("This is a README file!"));
   }
 
-  @Test
-  public void ifOneArgIsREADMEPrintFileElseExitWithZero(){
+  @Test//(expected = IllegalArgumentException.class)
+  public void ifOneArgIsNeitherREADMENorPrintThroeIllegalArgExceptionAndExitWithZero(){
       MainMethodResult result = invokeMain(Project1.class, "sree");
-      if(! result.getTextWrittenToStandardOut().equals("This is a README file!")){
-          assertThat(result.getExitCode(), equalTo(0));
+      assertThat(result.getExitCode(), equalTo(0));
       }
-  }
+
 
   @Test
   public void passAnyLengthArgumentAndIfREADMEAtOneOrTwoThenPrintfile(){
-      //MainMethodResult result = invokeMain(Project1.class, "-README", "-print");
+      MainMethodResult result = invokeMain(Project1.class, "-README", "-print");
       //MainMethodResult result = invokeMain(Project1.class,"-print","-README");
       //MainMethodResult result = invokeMain(Project1.class,"-print","-README","sree","123-123-1234");
-      MainMethodResult result = invokeMain(Project1.class,"-print","sree","123-123-1234","-README");
-      assertThat(result.getExitCode(), equalTo(0));
+      //MainMethodResult result = invokeMain(Project1.class,"-print","sree","123-123-1234","-README");
+      assertThat(result.getExitCode(), equalTo(1));
       //assertThat(result.getTextWrittenToStandardOut(), containsString("This is a README file!"));
   }
-
-  @Ignore
-  @Test
-  public void passingSevenCommandLineArguments() {
-    MainMethodResult result = invokeMain(Project1.class, "sree", "123-456-7890", "111-222-3333", "1/15/2020", "19:39", "01/2/2020", "1:03");
-    assertThat(result.getExitCode(), equalTo(1));
-    //assertThat(result.getTextWrittenToStandardError(), containsString("Missing command line arguments"));
-      MainMethodResult r = invokeMain();
+    @Ignore
+  @Test//(expected = IllegalNumberOfArgumentException.class)
+  public void onlyPrintOptionIsSentWithoutArgumemnts(){
+      MainMethodResult result = invokeMain(Project1.class,"-print");
+      assertThat(result.getExitCode(), equalTo(0));
   }
 
-
-
-  @Ignore
   @Test
-    public void passingArgsAndPrinting(){
-      MainMethodResult result = invokeMain(Project1.class,"sree", "111-222-3333", "000-999-8888", "1/15/2020 19:39", "1/15/2020 20:39");
-      assertThat(result.getExitCode(), equalTo(1));
-      assertThat(result.getTextWrittenToStandardError(), containsString("Missing command line arguments"));
+  public void validNameIsAStringFormat(){
+    //can have -, numbers, _, " ",., alphabets
+    // should not contain "special char"
+        MainMethodResult result = invokeMain(Project1.class, "-print", "-1SreeV2", "123-123-1234", "123-123-1234","1/15/2020", "19:39", "1/15/2020", "19:39" );
+        assertThat(result.getExitCode(),equalTo(1));
+      //assertThat(result.getTextWrittenToStandardOut(), containsString("sree"));
   }
-
-
 
 }
--- phonebill/src/main/java/edu/pdx/cs410J/sreev2/Project1.java
+++ phonebill/src/main/java/edu/pdx/cs410J/sreev2/Project1.java
@@ -10,6 +10,8 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Collection;
+import edu.pdx.cs410J.sreev2.NotSufficientArguments;
+import java.util.regex.Pattern;
 
 /**
  * The main class for the CS410J Phone Bill Project
@@ -19,70 +21,88 @@ import java.util.Collection;
 public class Project1 {
 
   private static final int minArgsLen = 7;
+  private static final String usage = "Expected input\n" +
+          "java edu.pdx.cs410J.<login-id>.Project1 [options] <args>\n" +
+          "<args> are (in this order):\n" +
+          "<customer>  <callerNumber>  <calleeNumber>  " +
+          "<start Date>  <start time>  " +
+          "<end Date>  <end time>  \n" +
+          "[options] = [-print -README] can appear in any order\n" +
+          "The <args> should be given in String format." +
+          "please give the date in format \"mm/dd/yyyy\" " +
+          "and time in format \"hh:mm\"";
 
   public static void main(String[] args) {
-
-    String usage = "Expected input\n" +
-            "java edu.pdx.cs410J.<login-id>.Project1 [-print -README] <args>\n" +
-            "<args> are (in this order):\n" +
-            "<customer>  <callerNumber>  <calleeNumber>  " +
-            "<start Date>  <start time>  " +
-            "<end Date>  <end time>  \n" +
-            "[options] may appear in any order\n" +
-            "The <args> should be given in String format." +
-            "please give the date in format \"mm/dd/yyyy\" " +
-            "and time in format \"hh:mm\"";
 
     var length = args.length;
 
     /*
     * If no Arguments are passed, then send error message and exit with exit-code 1
     * */
-
     if (length == 0) {
       System.err.println("Missing command line arguments");
       System.err.println(usage);
       System.exit(1);
     }
-
+/*
+* CASE 1: README
+* CASE 2: NO README
+* */
     else if (length <= 9) {
 
-      /*
-       * If in given arguments -README is present at locations 1 or 2
-       * then print the README.Txt file.
-       * DO NOT perform any operations.
-       * */
-
-      if (Arrays.asList(args).contains("-README") & Arrays.asList(args).indexOf("-README") < 2) {
-        try (InputStream readme = Project1.class.getResourceAsStream("README.txt")) {
-          BufferedReader reader = new BufferedReader(new InputStreamReader(readme));
-          String line = reader.readLine();
-          System.out.println(line);
-        } catch (IOException e) {
-          System.err.println(e);
-        }
+      if (Arrays.asList(args).contains("-README") &
+              Arrays.asList(args).indexOf("-README") < 2) {
+        printReadME();
         System.exit(1);
       }
-      //PhoneCall call = new PhoneCall(args);  // Refer to one of Dave's classes so that we can be sure it is on the classpath
-      //PhoneBill bill = new PhoneBill(args[0], call);
-      //System.out.println("call = "+call);
-      //System.out.println("bill = "+ bill);
-      //System.err.println("Missing command line arguments");
-      //for (String arg : args) {
-      //  System.out.println(arg);
-      //}
 
-      /*
-      * If README not present and print is present.
-      * then check if other required arguments are passed correctly and
-      * print the discription about the call.
-      * */
-      else {
-
-        System.exit(0);
-      }
+      else if(Arrays.asList(args).contains("-print") &
+                Arrays.asList(args).indexOf("-print") < 1 &
+              validateArgs(args)){
+          System.exit(1);
+        }
 
     }
+    // [no option] given and [print + no_arguments] and
+    // [print + few less arguments]
 
+    System.exit(0);
+  }
+
+  private static Boolean validateArgs(String[] args) {
+    String regexName = "[a-zA-Z0-9.-]*";
+    String regexPhoneNo = "^\\d{3}-\\d{3}-\\d{4}$";
+    String regexDate = "/^(0?[1-9]|1[0-2])\\/(0?[1-9]|1\\d|2\\d|3[01])\\/(19|20)\\d{2}$/";
+    String regexTime = "^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$";
+
+    if(args.length < 8){
+     //throw new IllegalNumberOfArgumentException(usage);
+     // throw new NotSufficientArguments(usage);
+      return false;
+    }
+
+    if(args.length == 8) {
+      if (Pattern.matches(regexName, args[1]) &
+              Pattern.matches(regexPhoneNo, args[2]) &
+              Pattern.matches(regexPhoneNo, args[3])){
+        return true;
+      }
+      else {
+        //throw new IllegalFormatOfDataException();
+        return false;
+      }
+    }
+    //System.err.println("Given Name in in wrong Fromat (should not contain ant special charecters)\nit can contain - and  _ and Numbers");
+    return false;
+  }
+
+  private static void printReadME() {
+    try (InputStream readme = Project1.class.getResourceAsStream("README.txt")) {
+      BufferedReader reader = new BufferedReader(new InputStreamReader(readme));
+      String line = reader.readLine();
+      System.out.println(line);
+    } catch (IOException e) {
+      System.err.println(e);
+    }
   }
 }
